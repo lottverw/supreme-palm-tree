eff362369788c74a120b0902d1c3abafb703abef
lotte <14766250+lottverw@users.noreply.github.com>
2024-11-12 08:57:22 +0100

    Updated some typescript errors

--- src/views/CommitView.vue
+++ src/views/CommitView.vue
@@ -2,14 +2,13 @@
 import { useCommits } from '@/composables/useCommits';
 import { useRoute } from 'vue-router';
     const route = useRoute();
-    const { commits } = useCommits(route.params.owner, route.params.repo)
+    const { commits } = useCommits(route.params.owner as string, route.params.repo as string)
 </script>
 
 
 <template>
     <h1>Repository {{ route.params.repo }}</h1>
-
-    <ul v-for="commit in commits">
+    <ul v-for="commit in commits" :key="commit.sha">
         <li>{{ commit.sha }}</li>
     </ul>
 
